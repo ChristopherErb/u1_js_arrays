a9dba4aa61c333d90194d38e9b5d203b17c4adfd
Christopher Erb <christophererb01@gmail.com>
2023-05-09 13:31:04 -0400

    final note commit

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log("popcorn")
+/* Arrays
+
+console.log(fruits)
+console.log(fruits[0])
+
+//How to get length of fruit array
+console.log(fruits.length)
+
+
+*/
+let friends = ["Moe", "Larry", "Curly"];
+let fruits = ["Apple", "Orange", "Grapes", "Tomato"]
+//.pop() removes final item
+//console.log(fruits.pop())
+//console.log(fruits)
+
+
+//.push() adds items into array
+//fruits.push("kiwi", "pineapple")
+//console.log(fruits)
+
+
+//.reverse() Reverses the array
+//fruits.reverse();
+//console.log(fruits);
+
+//.shift() removes the first item in the array
+//fruits.shift()
+//console.log(fruits)
+
+//.unshift() adds items to the beginning of the array
+//fruits.unshift("Date", "fig")
+//console.log(fruits)
+
+//.splice()  Add, Delete, Rename items
+//3 arguments,  Start, how far to go, what to do
+//fruits.splice(0, 1, "fuji apple")
+//console.log(fruits);
+
